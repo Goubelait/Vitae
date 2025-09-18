82fa8ea3ec378598e14f1f423c41b787374e4194
Anto <antoninweissgerber@gmail.com>
2025-09-18 15:10:17 +0200

    mise à jour layout pour utilisation font sur ios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import { Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-// Ensure logger config exists before Reanimated is loaded
 import "react-native-reanimated";
 import "../_lib/reanimated-logger-config";
 
@@ -12,26 +12,21 @@ import {
   ReanimatedLogLevel,
 } from "react-native-reanimated";
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "(tabs)",
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Inter_400Regular,
+    Inter_700Bold,
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -43,7 +38,6 @@ export default function RootLayout() {
   }, [loaded]);
 
   useEffect(() => {
-    // Configure Reanimated logger to prevent strict-mode warnings
     try {
       configureReanimatedLogger({
         level: ReanimatedLogLevel.warn,
