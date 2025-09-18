24814603098c77c54e62c7c46081b382d2fd2ec2
antowgb <antoninweissgerber@gmail.com>
2025-09-18 17:46:30 +0200

    ajoute de tous les sons

--- assets/PresetStyle.ts
+++ assets/PresetStyle.ts
@@ -3,7 +3,7 @@ export const presetStyles = [
     id: 1,
     name: "Sleep",
     icon: "bed",
-    subtitle: "1Hz",
+    subtitle: "150hz - 1Hz",
     sounds: {
       left: require("assets/sounds/sleep_left.mp3"),
       right: require("assets/sounds/sleep_right.mp3"),
@@ -13,30 +13,30 @@ export const presetStyles = [
     id: 2,
     name: "Relax",
     icon: "leaf",
-    subtitle: "8Hz",
+    subtitle: "150hz - 8Hz",
     sounds: {
-      left: require("assets/sounds/sleep_left.mp3"),
-      right: require("assets/sounds/sleep_right.mp3"),
+      left: require("assets/sounds/relax_left.mp3"),
+      right: require("assets/sounds/relax_right.mp3"),
     },
   },
   {
     id: 3,
     name: "Create",
     icon: "eye",
-    subtitle: "20Hz",
+    subtitle: "200Hz - 20Hz",
     sounds: {
-      left: require("assets/sounds/sleep_left.mp3"),
-      right: require("assets/sounds/sleep_right.mp3"),
+      left: require("assets/sounds/create_left.mp3"),
+      right: require("assets/sounds/create_right.mp3"),
     },
   },
   {
     id: 4,
     name: "Focus",
     icon: "bulb",
-    subtitle: "40Hz",
+    subtitle: "200Hz - 40Hz",
     sounds: {
-      left: require("assets/sounds/sleep_left.mp3"),
-      right: require("assets/sounds/sleep_right.mp3"),
+      left: require("assets/sounds/focus_left.mp3"),
+      right: require("assets/sounds/focus_right.mp3"),
     },
   },
 ];
